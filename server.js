f618ae614acdaf8a002666854bf3575efdc99a3b
ncrafe@gmail.com <ncrafe@gmail.com>
2018-11-08 17:54:25 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,176 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const uuidv4 = require('uuid/v4');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+const ObjectId = require('mongodb').ObjectId;
+const randomWord = require('random-word');
+
+let port = 7000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(express.static("web"))
+app.listen(port)
+
+console.log(`app listening on ${port}`)
+
+var url = 'mongodb://hangmanUser:derek99@ds155695.mlab.com:55695/hangman_game';
+
+// I'm using the random-word npm package,
+
+//connect to our database in mlad to store our game data through mongo
+MongoClient.connect(url, {
+    useNewUrlParser: true
+}, function(err, client) {
+
+    if (err) throw err;
+
+    let db = client.db('hangman_game').collection('new_hangman_game');
+
+
+    // routes
+    app.post('/hangman/word', function(req, res) {
+        let sessionID = req.body.sessionID;
+        // check to see if there is a sessionID,
+        // if not, create a new one
+        if (sessionID === null) {
+            sessionID = uuidv4();
+        }
+        let word = randomWord();
+        let maskedWord = word.replace(/./g, '*');
+        let gallowsSrc = 0;
+
+        let gameData = {
+            sessionID,
+            word,
+            maskedWord,
+            gallowsSrc,
+            guessesRemaining: 10,
+            correctGuesses: [],
+            incorrectGuesses: [],
+            gameWon: false,
+            gameLost: false
+        };
+        // insert the game, and send the gameID and sessionID
+        // back to the client
+        db.insertOne(gameData).then(data => {
+            // an _id field will automatically be created on the inserted
+            // document, and this value will be returned to you on the prop
+            // called 'insertedId'. You want to send this value (along with sessionID)
+            // back to the browser, so that it can store them, and send them
+            // along on all of the "guess letter" requests, so that we know
+            // who is playing (based on sessionID), and what game they
+            // are playing (based on gameID)
+            res.json({
+                gameID: data.insertedId,
+                sessionID,
+                maskedWord
+            });
+        });
+    });
+
+    app.post('/hangman/letter', function(req, res) {
+        let {
+            sessionID,
+            gameID,
+            letter
+        } = req.body;
+        let _id = ObjectId(gameID); // coercing this to the format the database understands
+
+        let findFilter = {
+            sessionID,
+            _id
+        };
+        // find the game doc based on the sessionID and gameID sent from the client
+        db.findOne(findFilter, (err, gameData) => {
+            let gameStatus = checkGuess(letter, gameData);
+            let updateFilter = {
+                $set: gameStatus
+            };
+            // update the game doc with the latest stats
+            db.updateOne({
+                _id
+            }, updateFilter, (err, updateRes) => {
+                // then send the current stats back to the client
+                res.send(gameStatus);
+            });
+        });
+    })
+
+    app.post('/hangman/records', function(req, res) {
+        let {
+            sessionID
+        } = req.body;
+
+        let gamesWon = 0;
+        let gamesLost = 0;
+
+        db.find({
+            sessionID
+        }).toArray().then(userGames => {
+            userGames.forEach(game => {
+                if (game.gameWon) {
+                    gamesWon++;
+                }
+
+                if (game.gameLost) {
+                    gamesLost++;
+                }
+            });
+
+            res.json({
+                gamesWon,
+                gamesLost
+            });
+        });
+    });
+});
+
+//check to see if the user's guess is correct or not and then run a command based off the result
+function checkGuess(letter, gameData) {
+    let {
+        word,
+        guessesRemaining,
+        correctGuesses,
+        incorrectGuesses,
+        gameWon,
+        gameLost,
+        maskedWord,
+        gallowsSrc
+    } = gameData;
+
+    var correct = false;
+    for (var i = 0; i < word.length; i++) {
+        if (word[i].toLowerCase() == letter.toLowerCase()) {
+            correct = true;
+            maskedWord = maskedWord.substr(0, i) + letter + maskedWord.substr(i + 1);
+        }
+    }
+    if (correct) {
+        correctGuesses.push(letter.toLowerCase());
+        if (maskedWord.indexOf('*') == -1) {
+            gameWon = true;
+        }
+    } else {
+        guessesRemaining--;
+        gallowsSrc++;
+        incorrectGuesses.push(letter.toLowerCase())
+        if (guessesRemaining < 1) {
+            gameLost = true;
+        }
+    }
+    //console.log(letter)
+    return {
+        correctGuesses,
+        incorrectGuesses,
+        guessesRemaining,
+        gameWon,
+        gameLost,
+        maskedWord,
+        gallowsSrc
+    }
+}
---
+++ web/app.js
@@ -0,0 +1,129 @@
+var app = angular.module("HangmanApp", []);
+app.controller("GameController", ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
+
+    //Gallows source images
+    var images = [
+        'images/0.jpg',
+        'images/1.jpg',
+        'images/2.jpg',
+        'images/3.jpg',
+        'images/4.jpg',
+        'images/5.jpg',
+        'images/6.jpg',
+        'images/7.jpg',
+        'images/8.jpg',
+        'images/9.jpg',
+        'images/10.jpg'
+    ]
+    var selectedWord = '';
+    $scope.incorrectLettersChosen = [];
+    $scope.correctLettersChosen = [];
+    $scope.guessesLeft = 10;
+    $scope.gallowsSrc = 0;
+    $scope.wins = 0;
+    $scope.loses = 0;
+    $scope.displayWord = '';
+    $scope.imageSrc = '';
+    $scope.input = {
+        letter: ''
+    }
+
+    //this function resets are varialbles and runs the new game
+    $scope.newGame = function() {
+        $scope.incorrectLettersChosen = [];
+        $scope.correctLettersChosen = [];
+        $scope.guessesLeft = 10;
+        $scope.gallowsSrc = 0;
+        $scope.displayWord = '';
+
+        $scope.imageSrc = images[$scope.gallowsSrc];
+        //console.log($scope.imageSrc);
+        document.getElementById("winLose").innerHTML = '';
+
+        let data = {
+            sessionID: localStorage.getItem('sessionID')
+        };
+        //When a new game starts we send to our database the game and session ID so that we whos playing
+        //-in case multiple users are playing at the same time
+        //-then we display a hidden random word to the UI
+        $http.post('/hangman/word', data).then(function(response) {
+            localStorage.setItem('gameID', response.data.gameID);
+            localStorage.setItem('sessionID', response.data.sessionID);
+            $scope.displayWord = response.data.maskedWord;
+        });
+        //keeps tracks of the users wins and loses and displays them at the beginning of each game
+        $http.post('/hangman/records', data).then(function(response) {
+            $scope.wins = response.data.gamesWon;
+            $scope.loses = response.data.gamesLost;
+        });
+
+
+
+    }
+    //user presses submit and sends there guess to be tested
+    $scope.letterChosen = function() {
+        //check to see if we have any invalid entrys
+        if (($scope.input.letter === '') || ($scope.input.letter.length > 1)) {
+
+            $scope.input.letter = "";
+            document.getElementById("invalid").innerHTML = "INVALD ENTRY !";
+
+        } //if input is valid run as normal
+        else {
+            document.getElementById("invalid").innerHTML = '';
+            //now we send our guess and it check to see if it is correct or incorrect based on our random word
+            let url = '/hangman/letter';
+
+            let data = {
+                sessionID: localStorage.getItem('sessionID'),
+                gameID: localStorage.getItem('gameID'),
+                letter: $scope.input.letter
+            };
+
+            let guessData = JSON.stringify(data)
+            let config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+            //post our data to the server and get our response and diplay our data to the UI
+            $http.post(url, guessData, config).then(function(result) {
+                $scope.input.letter = "";
+                //console.log(result.data);
+                $scope.displayWord = result.data.maskedWord;
+                $scope.guessesLeft = result.data.guessesRemaining;
+                $scope.gallowsSrc = result.data.gallowsSrc;
+
+                $scope.correctLettersChosen = result.data.correctGuesses;
+                let incorrectGuesses = result.data.incorrectGuesses;
+                let winner = result.data.gameWon;
+                let loser = result.data.gameLost;
+                //check whether the user won or not then display a message based off the result
+                if (result.data.gameWon) {
+                    document.getElementById("winLose").innerHTML = "You WON";
+                    document.getElementById("newGame").style.display = "block";
+                }
+
+                if (result.data.gameLost) {
+                    document.getElementById("winLose").innerHTML = "You LOST";
+                    document.getElementById("newGame").style.display = "block";
+                }
+                //replaces our gallows image source when whenever we make an incorrect guess
+                incorrectGuesses.forEach(function(element) {
+                    if (!($scope.incorrectLettersChosen.includes(element))) {
+                        $scope.incorrectLettersChosen.push(element.toLowerCase());
+                        $scope.imageSrc = images[$scope.gallowsSrc];
+                    }
+
+                })
+
+            }, function(error) {
+                console.log(error);
+            });
+
+
+        }
+    }
+
+    $scope.newGame();
+}])
